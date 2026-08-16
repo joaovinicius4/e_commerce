1488fb33f37a55b31af1526654244b1a765dbe5d
joaovinicius4 <joaoruas111@gmail.com>
2026-08-16 15:07:06 -0300

    add database em clientes

--- entities/cliente.py
+++ entities/cliente.py
@@ -1,5 +1,20 @@
-class Cliente:
-    def __init__(self, id_cliente: int, nome: str, email: str):
-        self.id_cliente = id_cliente
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Cliente(Base):
+    __tablename__ = 'clientes'
+
+    id_cliente = Column(Integer, primary_key=True, autoincrement=True)
+    cpf = Column(String(11), unique=True, nullable=False)           
+    nome = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    endereco = Column(String(100), nullable=False)
+
+    def __init__(self, cpf: str, nome: str, email: str, endereco: str):
+        self.cpf = cpf
         self.nome = nome
         self.email = email
+        self.endereco = endereco
+
+    def __str__(self) -> str:
+        return f"Cliente: {self.nome} | CPF: {self.cpf} | E-mail: {self.email}"
