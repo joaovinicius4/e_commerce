e6cad3ce14d6e0d80e24ff9e019ec950a0a2b537
joaovinicius4 <joaoruas111@gmail.com>
2026-08-16 14:57:07 -0300

    add database em produtos

--- entities/produto.py
+++ entities/produto.py
@@ -1,13 +1,20 @@
-class Produto:
-    def __init__(self, id_produto: int, nome: str, preco: float, estoque: int):
-        
-        self.id_produto = id_produto
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Produto(Base):
+    __tablename__ = 'produtos' 
+    id_produto = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String, nullable=False)
+    preco = Column(Float, nullable=False)
+    estoque = Column(Integer, nullable=False)
+
+    def __init__(self, nome: str, preco: float, estoque: int):
         self.nome = nome
         self.preco = preco
         self.estoque = estoque
 
     def __str__(self) -> str:
-        return f"Produto: {self.nome}, Codigo: {self.id_produto}, Preço: {self.preco}, Quantidade em estoque: {self.estoque}"
+        return f"Produto: {self.nome}, Codigo: {self.id_produto}, Preço: R$ {self.preco:.2f}, Quantidade em estoque: {self.estoque}"
 
     def adicionar_estoque(self, quantidade: int) -> None:
         if quantidade > 0:
@@ -21,4 +28,3 @@ class Produto:
             raise ValueError(f"Estoque insuficiente. Disponível: {self.estoque}, solicitado: {quantidade}")
             
         self.estoque -= quantidade
-        
